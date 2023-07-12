41c50f6ca8c8563c7353e4af68627f6fdf2c6312
Thiago Dantas <thiagoo.dantas11@gmail.com>
2023-07-12 19:24:51 +1000

    fetch code added to javaScript file

---
+++ assets/js/script.js
@@ -0,0 +1,34 @@
+
+const ApiKey = ("c32a31c6a7ffcbea5bdd726183442f5b");
+
+
+
+
+
+
+// const searchButton = document.getElementById("search-button");
+// const placeInput = document.getElementById("place-input");
+
+// searchButton.addEventListener("click", function () {
+//     var placeName = placeInput.value;
+//     var requestUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + placeName + "&appid=" + ApiKey;
+//     fetch(requestUrl)
+//         .then(function (response) {
+//             return response.jason();
+//         })
+//         .then(function (data) {
+//             console.log(data);
+//     })
+// })
+
+
+var city;
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + ApiKey;
+
+fetch(queryURL)
+    .then(function (response) {
+        return response.jason();
+    })
+    .then(function (data) {
+        console.log(data);
+    })
