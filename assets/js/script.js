32b0c35bfe5298bcebada9f2f0aed6f5f30485fa
Thiago Dantas <thiagoo.dantas11@gmail.com>
2023-07-13 18:24:28 +1000

    fixing fetching error

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,53 @@
 
 const ApiKey = ("c32a31c6a7ffcbea5bdd726183442f5b");
 
+const searchButton = document.getElementById("search-button");
+const placeInput = document.getElementById("place-input");
+
+searchButton.addEventListener("click", function () {
+    var placeName = placeInput.value;
+    var requestUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + placeName + "&appid=" + ApiKey;
+    // var queryURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${ApiKey}`
+    fetch(requestUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+    })
+})
 
 
 
 
 
-// const searchButton = document.getElementById("search-button");
-// const placeInput = document.getElementById("place-input");
 
-// searchButton.addEventListener("click", function () {
-//     var placeName = placeInput.value;
-//     var requestUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + placeName + "&appid=" + ApiKey;
-//     fetch(requestUrl)
-//         .then(function (response) {
-//             return response.jason();
-//         })
-//         .then(function (data) {
-//             console.log(data);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var city = "London"
+// //var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + ApiKey;
+// var queryURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${ApiKey}`
+
+// fetch(queryURL)
+//     .then(function (response) {
+//         return response.json();
 //     })
-// })
-
-
-var city;
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + ApiKey;
-
-fetch(queryURL)
-    .then(function (response) {
-        return response.jason();
-    })
-    .then(function (data) {
-        console.log(data);
-    })
+//     .then(function (data) {
+//         console.log(data);
+//     })
